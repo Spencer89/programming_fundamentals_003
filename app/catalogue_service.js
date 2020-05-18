85804392068e251914d9b06808e47bb783289088
Nichole Spencer <nic_spencer@hotmail.co.uk>
2020-05-18 11:21:35 +0100

    Added tests and completed Q1 and Q2

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -2,163 +2,192 @@
 const catalogue = [
 
   {
-            title:"The Catcher in the Rye",
-            author: "J.D. Salinger",
-            quantity: 10
-          },
-  
-            {
-              title:"Dracula",
-              author:"Bram Stoker",
-              quantity:0
-            },
-  
-  
-            {
-              title:"Between the Assassinations",
-              author:"Aravind Adiga",
-              quantity:9
-            },
-  
-  
-            {
-              title:"Wolf Hall",
-              author:"Hilary Mantel",
-              quantity: 33
-            },
-  
-  
-            {
-              title:"Bringing Up The Bodies",
-              author:"Hilary Mantel",
-              quantity:11
-            },
-  
-  
-           {
-              title:"A Place of Greater Safety",
-              author:"Hilary Mantel",
-              quantity:11
-            },
-  
-  
-            {
-              title:"Giving Up The Ghost",
-              author:"Hilary Mantel",
-              quantity:8
-            },
-  
-            {
-              title:"The Assassination of Margaret Thatcher",
-              author:"Hilary Mantel",
-              quantity:43
-            },
-  
-            {
-              title:"The Yellow Wallpaper",
-              author:"Charlotte Perkins Gilman",
-              quantity:12,
-            },
-  
-            {
-              title:"Conversations With Friends",
-              author:"Sally Rooney",
-              quantity:1,
-            },
-  
-            {
-              title:"Normal People",
-              author:"Sally Rooney",
-              quantity:2,
-            },
-  
-            {
-              title:"Everything I Never Told You",
-              author:"Celeste Ng",
-              quantity:6,
-            },
-  
-            {
-              title:"2666",
-              author:"Robert Bolaño",
-              quantity:17,
-            },
-  
-            {
-              title:"By Night In Chile",
-              author: "Robert Bolaño",
-              quantity:8,
-            },
-  
-            {
-              title:"A Tale of Two Cities",
-              author:"Charles Dickens",
-              quantity:3,
-            },
-  
-            {
-              title: "Oliver Twist",
-              author: "Charles Dickens",
-              quantity: 7
-            },
-  
-            {
-              title:"Great Expectations",
-              author:"Charles Dickens",
-              quantity:1,
-            },
-  
-            {
-              title:"The Blind Assassin",
-              author:"Margaret Atwood",
-              quantity:8,
-            },
-  
-            {
-              title:"Why Be Happy When You Could Be Normal?",
-              author:"Jeanette Winterson",
-              quantity:19,
-            },
-  
-            {
-              title:"The Origin Of Species",
-              author:"Charles Darwin",
-              quantity:50,
-            }
-  
-  ];
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    quantity: 10
+  },
 
-  function countBooksByAuthor(obj) {
- 
-    let count = 0;
-  
-    for(let i = 0; i < catalogue.length; i++) {
-      if(catalogue[i].author === obj) {
-        count++;
-      }
-    }
-    return count;
+  {
+    title: "Dracula",
+    author: "Bram Stoker",
+    quantity: 0
+  },
+
+
+  {
+    title: "Between the Assassinations",
+    author: "Aravind Adiga",
+    quantity: 9
+  },
+
+
+  {
+    title: "Wolf Hall",
+    author: "Hilary Mantel",
+    quantity: 33
+  },
+
+
+  {
+    title: "Bringing Up The Bodies",
+    author: "Hilary Mantel",
+    quantity: 11
+  },
+
+
+  {
+    title: "A Place of Greater Safety",
+    author: "Hilary Mantel",
+    quantity: 11
+  },
+
+
+  {
+    title: "Giving Up The Ghost",
+    author: "Hilary Mantel",
+    quantity: 8
+  },
+
+  {
+    title: "The Assassination of Margaret Thatcher",
+    author: "Hilary Mantel",
+    quantity: 43
+  },
+
+  {
+    title: "The Yellow Wallpaper",
+    author: "Charlotte Perkins Gilman",
+    quantity: 12,
+  },
+
+  {
+    title: "Conversations With Friends",
+    author: "Sally Rooney",
+    quantity: 1,
+  },
+
+  {
+    title: "Normal People",
+    author: "Sally Rooney",
+    quantity: 2,
+  },
+
+  {
+    title: "Everything I Never Told You",
+    author: "Celeste Ng",
+    quantity: 6,
+  },
+
+  {
+    title: "2666",
+    author: "Robert Bolaño",
+    quantity: 17,
+  },
+
+  {
+    title: "By Night In Chile",
+    author: "Robert Bolaño",
+    quantity: 8,
+  },
+
+  {
+    title: "A Tale of Two Cities",
+    author: "Charles Dickens",
+    quantity: 3,
+  },
+
+  {
+    title: "Oliver Twist",
+    author: "Charles Dickens",
+    quantity: 7
+  },
+
+  {
+    title: "Great Expectations",
+    author: "Charles Dickens",
+    quantity: 1,
+  },
+
+  {
+    title: "The Blind Assassin",
+    author: "Margaret Atwood",
+    quantity: 8,
+  },
+
+  {
+    title: "Why Be Happy When You Could Be Normal?",
+    author: "Jeanette Winterson",
+    quantity: 19,
+  },
+
+  {
+    title: "The Origin Of Species",
+    author: "Charles Darwin",
+    quantity: 50,
   }
- 
 
-function checkBookByTitle(title) {
+];
+//Q1 - COMPLETE
+function countBooksByAuthor(obj) {
 
+  let count = 0;
+
+  for (let i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].author === obj) {
+      count++;
+    }
+  }
+  return count;
 }
+
+//Q2 - COMPLETE
+function checkBookByTitle(title) {
+  for (let i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].title === title) { return true; }
+
+  }
+}
+
+/*Q3 - ***INCOMPLETE***
+This function should receive a letter as an argument (e.g. "H") and return the total number
+ of books that begin with this letter. Note that the quantity is not relevant here.
+ Ensure it passes regardless of case*/
 
 function countBooksByFirstLetter(letter) {
+  let count = 0;
+  for (let i = 0; i < catalogue.length; i++) {
 
+    if (catalogue[i].charAt(0) === letter) {
+      count++
+    }
+  }
+  return count;
 }
+
+/*Q4 - INCOMPLETE
+This function should receive a title as an argument (e.g. "The Origin of Species") and
+ return the quantity of this item which is in stock*/
 
 function getQuantity(title) {
-
+ for (let i=0; i< catalogue.length; i++){
+   return catalogue[title].quantity
+ }
 }
+
+/*Q5 *no test*
+This function should receive an author as an argument (e.g. "Robert Bolaño") and return
+ an array of books*/
 
 function getBooksByAuthor(author) {
 
 }
-
+/*Q6
+This function should receive a title and a quantity as an argument
+ (e.g. "By Night In Chile" and 4) and return `true` if there are at least as many books
+  in stock as the given quantity, and false otherwise.*/
 function checkQuantity(title, quantity) {
- 
+  
+
 }
 
 
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,5 +1,5 @@
 const catalogueService = require("../app/catalogue_service");
-
+//Q1 TEST
 describe("catalogueService", () => {
   describe("catalogueService.countBooksByAuthor", () => {
     test("returns the total number of books written by the given author", () => {
@@ -10,4 +10,54 @@ describe("catalogueService", () => {
   });
 
 })
+
+//Q2 TEST
+describe("catalogueService", () => {
+describe("catalogueService.checkBookByTitle", () => {
+  test("returns true if the book exists", () => {
+    expect(
+      catalogueService.checkBookByTitle(
+        "The Assassination of Margaret Thatcher"
+      )
+    ).toBe(true);
+  });
+});
+});
+
+//Q3 TEST
+describe.only("catalogueService", () => {
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("receive a letter as an arguments and return the total number of books that begin with that letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("T")).toBe(5);
+      expect(catalogueService.countBooksByFirstLetter("G")).toBe(2);
+    });
+  });
+
+})
+
+//Q4 TEST
+describe("catalogueService", () => {
+  describe("catalogueService.getQuantity", () => {
+    test("receives a book title and returns the number of copies in stock", () => {
+      expect(catalogueService.getQuantity("Normal People")).toBe(2);
+      expect(catalogueService.getQuantity("Great Expectations")).toBe(1);
+    });
+  });
+
+})
+
+//Q5 TEST***
+
+
+//Q6 TEST
+describe("catalogueService", () => {
+  describe("catalogueService.checkQuantity", () => {
+    test("recieves a title and a quantity as an argument and returns true if there are at least that many books in stock, and false if not", () => {
+      expect(catalogueService.checkQuantity("Normal People", 1)).toBe(true);
+      expect(catalogueService.checkQuantity("Great Expectations", 50)).toBe(false);
+    });
+  });
+
+})
+
 
